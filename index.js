545bee9d7b619d2fee8859d2f3e75925a6c80f50
rashedrk <rashedrk001@gmail.com>
2023-05-04 13:43:17 +0600

    get recipe by id added

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var cors = require('cors')
 const port = 3000
 
 const chef = require('./data/chef/chef.json')
-
+const recipes = require('./data/recipes/recipes.json')
 app.use(cors())
 
 app.get('/', (req, res) => {
@@ -21,6 +21,14 @@ app.get('/chef/:id',(req,res) => {
     const selectedChef = chef.find(element => element.chef_id === id);
     res.send(selectedChef);
 })
+
+//get recipe by chef id
+app.get('/recipes/:id', (req,res) => {
+    const id = req.params.id;
+    const selectedRecipes = recipes.filter( n => n.chef_id === id);
+    res.send(selectedRecipes);
+})
+
 app.listen(port, () => {
     console.log(`Chef Discoveries listening on port ${port}`)
 })
